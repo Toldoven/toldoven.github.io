a4e370000849ec82e6a9e7834d4237bd5261a4ec
Toldoven <toldoven@proton.me>
2024-09-25 19:23:13 +0300

    Fix SEO + Fix build

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,10 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 
-import icon from "astro-icon";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://toldoven.com',
-  integrations: [tailwind(), mdx(), icon()]
+  integrations: [tailwind(), mdx(), sitemap()]
 });
